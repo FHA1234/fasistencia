d61b31c09d7ce7e75ec3532a5785798725e625d0
FHA1234 <117729314+FHA1234@users.noreply.github.com>
2025-08-28 09:11:17 +0200

    flush.js

---
+++ netlify/functions/flush.js
@@ -0,0 +1,13 @@
+const { resp, gsPost } = require('./_fetch');
+
+exports.handler = async (event) => {
+  try {
+    if (event.httpMethod !== 'POST') return resp(405, { error: 'METHOD_NOT_ALLOWED' });
+    const { pw = '' } = JSON.parse(event.body || '{}');
+    const data = await gsPost({ action: 'flush', pw });
+    return resp(200, data);
+  } catch (e) {
+    console.error('flush error:', e);
+    return resp(502, { error: 'BACKEND_UNAVAILABLE' });
+  }
+};
