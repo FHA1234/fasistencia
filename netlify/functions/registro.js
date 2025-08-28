9d84872e183a66a90fb875ee7a7a8312e7593788
FHA1234 <117729314+FHA1234@users.noreply.github.com>
2025-08-28 14:32:23 +0200

    registro.js

--- netlify/functions/registro.js
+++ netlify/functions/registro.js
@@ -1,15 +1,15 @@
-// netlify/functions/flush.js
+// netlify/functions/registro.js
 const { resp, gsPost } = require('./_fetch');
 
 exports.handler = async (event) => {
   try {
     if (event.httpMethod !== 'POST') return resp(405, { error: 'METHOD_NOT_ALLOWED' });
-    const { pw = '' } = JSON.parse(event.body || '{}');
-    // Apps Script nuevo: POST { action:'flush', pw }
-    const data = await gsPost({ action: 'flush', pw });
+    const payload = JSON.parse(event.body || '{}');
+    // Apps Script nuevo: POST { action:'registro', ... }
+    const data = await gsPost({ action: 'registro', ...payload });
     return resp(200, data);
   } catch (e) {
-    console.error('flush error:', e);
+    console.error('registro error:', e);
     return resp(502, { error: 'BACKEND_UNAVAILABLE' });
   }
 };
